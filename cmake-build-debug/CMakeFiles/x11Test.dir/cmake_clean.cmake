601c2470a8dbb8efc2965ababc36595562a65fac
Dylan Bertsch <dbertsc1@students.kennesaw.edu>
2020-01-02 17:54:58 -0500

    Basic sanity check of X11

---
+++ cmake-build-debug/CMakeFiles/x11Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/x11Test.dir/main.cpp.o"
+  "x11Test"
+  "x11Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/x11Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+int main() {
+
+    Display *d;
+    int s;
+    Window w;
+    XEvent e;
+
+    /* open connection with the server */
+    d=XOpenDisplay(NULL);
+    if(d==NULL) {
+        printf("Cannot open display\n");
+        exit(1);
+    }
+    s=DefaultScreen(d);
+    w=XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
+                          BlackPixel(d, s), WhitePixel(d, s));
+    Atom delWindow = XInternAtom( d, "WM_DELETE_WINDOW", 0 );
+    XSetWMProtocols(d , w, &delWindow, 1);
+    XSelectInput(d, w, ExposureMask | KeyPressMask);
+    XMapWindow(d, w);
+    while(1) {
+        XNextEvent(d, &e);
+        /* draw or redraw the window */
+        if(e.type==Expose) {
+            //This is where the drawing or rendering happens
+            XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+        }
+        /* exit on key press */
+        if(e.type==KeyPress)
+            break;
+
+        // Handle Windows Close Event
+        if(e.type==ClientMessage)
+            break;
+    }
+
+    /* destroy our window */
+    XDestroyWindow(d, w);
+
+    /* close connection to server */
+    XCloseDisplay(d);
+    return 0;
+}
