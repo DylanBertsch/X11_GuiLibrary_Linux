dcbac1432181e91d92d6a1650391f20efa07c274
Dylan Bertsch <dbertsc1@students.kennesaw.edu>
2020-01-02 18:20:58 -0500

    Added XColor object to allow graphics to be drawn in different colors.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     Display *d;
     int s;
     Window w;
+    GC gc;
     XEvent e;
 
     /* open connection with the server */
@@ -21,13 +22,22 @@ int main() {
     Atom delWindow = XInternAtom( d, "WM_DELETE_WINDOW", 0 );
     XSetWMProtocols(d , w, &delWindow, 1);
     XSelectInput(d, w, ExposureMask | KeyPressMask);
+    gc = XCreateGC(d,w,NULL,NULL);
+    XColor color_cell;
+    color_cell.flags = DoRed | DoGreen | DoBlue;
+    color_cell.red = 10000;
+    color_cell.green = 40000;
+    color_cell.blue = 60000;
     XMapWindow(d, w);
     while(1) {
         XNextEvent(d, &e);
         /* draw or redraw the window */
         if(e.type==Expose) {
             //This is where the drawing or rendering happens
-            XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+            XSetForeground(d,gc,color_cell.pixel);
+            XFillRectangle(d, w, gc, 20, 20, 10, 10);
+
+            XDrawLine(d,w,gc,0,0,120,120);
         }
         /* exit on key press */
         if(e.type==KeyPress)
